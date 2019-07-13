e60d00e76479b1592f373986c2eb0397d51238f5
DerpyNewbie <derpynewbie@gmail.com>
2019-07-14 02:50:32 +0900

    fix compile error

--- Source/FPSGame/Private/FPSGameState.cpp
+++ Source/FPSGame/Private/FPSGameState.cpp
@@ -7,16 +7,8 @@
 
 void AFPSGameState::MulticastOnMissionComplete_Implementation(APawn* InstigatorPawn, bool bIsSucceed)
 {
-	//for (FConstPawnIterator i = GetWorld()->GetPawnIterator(); i; i++)
-	//{
-	//	APawn* pawn = i->Get();
-	//	if (pawn || pawn->IsLocallyControlled())
-	//	{
-	//		pawn->DisableInput(nullptr);
-	//		return;
-	//	}
-	//}
-	UE_LOG(LogTemp, Verbpse, TEXT("Executing MulticastOnMissionComplete"));
+
+	UE_LOG(LogTemp, Verbose, TEXT("Executing MulticastOnMissionComplete"));
 	for (FConstPlayerControllerIterator i = GetWorld()->GetPlayerControllerIterator(); i; i++)
 	{
 		AFPSPlayerController* pc = Cast<AFPSPlayerController>(i->Get());
